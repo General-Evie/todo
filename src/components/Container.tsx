448ded15f691e51c9db414491a4b601cde5b2e05
Evie Dickman <GeneralEvie9@outlook.com>
2023-09-15 09:25:18 -0500

    todo start

---
+++ src/components/Container.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Container() {
+    return (
+        <div>
+
+            Todo
+
+            {/* Add dynamic number  */} items left 
+
+            All
+            Active
+            Completed
+
+            Clear Completed
+
+            Drag and drop to reorder list
+
+            <div className="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                Coded by <a href="#">Your Name Here</a>.
+            </div>
+        </div>
+    )
+}
+
+export default Container
